25e34872b316f67b29b16d170abb6ddac3b601b6
a8081 <amartinezr98@protonmail.com>
2019-12-02 19:09:26 +0100

    Update!

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://decidejoker.herokuapp.com'
+BASEURL = 'http://decidejokerci.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
